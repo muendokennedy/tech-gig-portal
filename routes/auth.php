47b065350f0438c1037aa70d606d43ca767dd155
muendokennedy <kennedymunyao999@gmail.com>
2023-08-08 19:00:28 +0300

    Created middleware groups of auth and guest http requests

--- routes/auth.php
+++ routes/auth.php
@@ -3,14 +3,19 @@ use App\Http\Controllers\UserController;
 
 // User authentication route page
 
-// Render the user or the create form page
-Route::get('/register', [UserController::class, 'create'])->name('user.create');
-// Render the login form
-Route::get('/login', [UserController::class, 'login'])->name('user.login');
-// Create the user in the database
-Route::post('/register', [UserController::class, 'store'])->name('user.store');
-// Login the user
-Route::post('/login', [UserController::class, 'loginUser'])->name('login.user');
-// Logout the user
-Route::post('/logout', [UserController::class, 'logout'])->name('user.logout');
+Route::middleware('auth')->group(function(){
+    // Logout the user
+    Route::post('/logout', [UserController::class, 'logout'])->name('user.logout');
+
+});
+Route::middleware('guest')->group(function(){
+    // Render the user or the create form page
+    Route::get('/register', [UserController::class, 'create'])->name('user.create');
+    // Create the user in the database
+    Route::post('/register', [UserController::class, 'store'])->name('user.store');
+    // Render the login form
+    Route::get('/login', [UserController::class, 'login'])->name('user.login');
+    // Login the user
+    Route::post('/login', [UserController::class, 'loginUser'])->name('login.user');
+});
 
--- routes/web.php
+++ routes/web.php
@@ -18,20 +18,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ListingController::class, 'index'])->name('listings.index');
 // Show a particular lising
 Route::get('/listings/{listing}/show', [ListingController::class, 'show'])->name('listing.show');
-// Create a listing
-Route::get('/listings/create', [ListingController::class, 'create'])->name('listing.create')->middleware('auth');
-// Store a listing to the DB
-Route::post('/listings/store', [ListingController::class, 'store'])->name('listing.store');
-// Edit a single listing
-Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->name('listing.edit');
-// Update a single listing to the database
-Route::put('/listings/{listing}/update', [ListingController::class, 'update'])->name('listing.update');
-// Delete a single listing
-Route::delete('/listings/{listing}/delete', [ListingController::class, 'destroy'])->name('listing.destroy');
-// Logged in user can manage listings
-Route::get('/listings/manage', [ListingController::class, 'manage'])->name('listings.manage');
+
+Route::middleware('auth')->group( function(){
+
+    // Create a listing
+    Route::get('/listings/create', [ListingController::class, 'create'])->name('listing.create');
+    // Store a listing to the DB
+    Route::post('/listings/store', [ListingController::class, 'store'])->name('listing.store');
+    // Edit a single listing
+    Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->name('listing.edit');
+    // Update a single listing to the database
+    Route::put('/listings/{listing}/update', [ListingController::class, 'update'])->name('listing.update');
+    // Delete a single listing
+    Route::delete('/listings/{listing}/delete', [ListingController::class, 'destroy'])->name('listing.destroy');
+    // Logged in user can manage listings
+    Route::get('/listings/manage', [ListingController::class, 'manage'])->name('listings.manage');
+
+});
 
 require_once __DIR__ . '/auth.php';
+
+
 
 
 
